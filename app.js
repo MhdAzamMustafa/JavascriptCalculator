bc9f2c7a001ab4e2d875f87c59d9fc5deb664c95
Muhammad Azam Mustafa <64996861+MhdAzamMustafa@users.noreply.github.com>
2020-06-27 23:59:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+}
+function clear(){
+    var result = document.getElementById("clear");
+    result.value = "";
+}
